5f8f16cfd9104d14cc896ddf1dd5241377365241
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-03-25 17:50:22 -0700

    Try listdir

--- test.py
+++ test.py
@@ -8,12 +8,4 @@ model.fit(x=[[1]], y=[1])
 
 model.save("ram://test")
 
-for root, _, filenames in tf.io.gfile.walk("ram://test"):
-    for filename in filenames:
-        path = root + "/" + filename
-        print(f"root: {root}")
-        print(f"filename: {filename}")
-        print(f"path: {path}")
-        print("exists: ", tf.io.gfile.exists(path))
-        with tf.io.gfile.GFile(path, mode="rb") as f:
-            print(f.size())
+print(tf.io.gfile.listdir("ram://test")
