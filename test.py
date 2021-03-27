fe62a8402f4b1a495c5b45c8435da6ed66c4d7da
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-03-27 12:05:24 -0500

    make nice

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-from unittest.mock import patch
+# Silence TF noise
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+
+# Create a TF specific implementation of join
+# this would go in tf.io.gfile.join
 from os import path
 join_orig = path.join
 from posixpath import join as urljoin
@@ -9,14 +15,15 @@ def join(*paths):
         return urljoin(*paths)
     return join_orig(*paths)
 
-with patch("os.path.join", new=join):
+
+# define a test that uses savemodel and then walk to iterate over the saved data
+def test():
     import os
     import tensorflow as tf
 
-
-    model = tf.keras.Sequential([tf.keras.Input((1,)), tf.keras.layers.Dense(1)])
+    model = tf.keras.Sequential([tf.keras.layers.InputLayer((1,)), tf.keras.layers.Dense(1)])
     model.compile(loss="mse")
-    model.fit(x=[[1]], y=[1])
+    model.fit(x=[[1]], y=[1], verbose=0)
 
     model.save("ram://test")
 
@@ -24,4 +31,22 @@ with patch("os.path.join", new=join):
         for filename in filenames:
             path = os.path.join(root, filename)
             with tf.io.gfile.GFile(path, mode="rb") as f:
-                print(f.size())
+                f.size()
+
+# test with and without the hack
+
+try:
+    test()
+    print("PASSED WITHOUT HACK")
+except:
+    print("FAILED WITHOUT HACK")
+
+
+from unittest.mock import patch
+with patch("os.path.join", new=join):
+    # Test with the hack
+    try:
+        test()
+        print("PASSED WITH HACK")
+    except:
+        print("FAILED WITH HACK")
